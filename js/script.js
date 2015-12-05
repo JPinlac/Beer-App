3297638691cf8e3e75b53893f12b68d15381aef6
Jonathan <jonppinlac@gmail.com>
2015-12-05 13:58:26 -0500

    api testing framework

--- js/script.js
+++ js/script.js
@@ -16,14 +16,20 @@ app.config(['$routeProvider', function($routeProvider){
 }]);
 
 
-app.controller('searchController', function($scope, getBeer){
-
+app.controller('searchController', function($scope, beerService){
+    $scope.submitSearch = function(searchTerm) {
+        beerService.getBeer(searchTerm);
+    }
 
 });
 
 
-app.factory('getBeer', function($http){
+app.factory('beerService', function($http){
     service={};
+    service.getBeer = function(searchTerm){
+        $http.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&order=viewCount&publishedAfter=2015-01-01T00%3A00%3A00Z&publishedBefore=2015-12-31T00%3A00%3A00Z&q=beer&type=video&videoCategoryId=10&videoDuration=short&videoSyndicated=true&key=AIzaSyAcH5lbBeE0d_PovUz8XHtSj2dNvEzTauY').success(function(response){
+            console.log(response);
+        })
+    }
     return service;
 });
-
